45f7619f7215d7738fb5eb060f6dda5086edda8e
cs17b013 <cs17b013@iittp.ac.in>
2019-08-31 23:33:55 +0530

    linking webpages

--- asap-backend/client/src/Components/Navigation_Bar.js
+++ asap-backend/client/src/Components/Navigation_Bar.js
@@ -3,12 +3,16 @@ import {Button} from 'react-bootstrap';
 import  {Navbar} from 'react-bootstrap';
 //import  {Form} from 'react-bootstrap';
 import  {NavDropdown} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 //import {FormControl} from 'react-bootstrap';
 import {Nav} from 'react-bootstrap';
 import {ButtonGroup} from 'react-bootstrap';
 import logo from '../project_logo.JPG';
 import {Image} from 'react-bootstrap';
 import '../App.css';
+import SignUp from './SignUp';
+import Login from './Login';
+
 function Navigation_bar(){
 return(
 <Navbar expand="lg" variant="dark" className="navbar">
@@ -35,10 +39,10 @@ return(
       <Button variant="outline-success">Search</Button>
     </Form> */}
     <ButtonGroup className="mr-2" aria-label="First group">
-      <Button variant="outline-light">Sign up</Button>
+      <Button variant="outline-light"><Link to="/SignUp">Sign up</Link></Button>
     </ButtonGroup>
     <ButtonGroup className="mr-2" aria-label="Second group">
-      <Button variant="outline-light">Login</Button>
+      <Button variant="outline-light"><Link to="/Login">Login</Link></Button>
     </ButtonGroup>
 
   </Navbar.Collapse>
